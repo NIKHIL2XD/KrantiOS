a4a42a342e3755a53d24edc1b8d00cb914b7849e
NIKHIL2XD <nikhilmansotraxd@gmail.com>
2024-07-16 01:22:59 +0530

    fixed time, date

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const welcome = document.querySelector(".welcome");
 crossIcon.addEventListener("click", () => {
   welcome.style.display = "none";
 })
+
+var currentTime = new Date().toLocaleString();
+var timeText = document.querySelector("#time");
+timeText.innerHTML = currentTime
+
+setInterval(function () {
+  document.querySelector("#time").innerHTML = new Date().toLocaleString();
+}, 1000);
